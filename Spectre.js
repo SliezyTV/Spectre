9c9d3de9c94cea2e4a880ae9269f64e51f8ecf52
Ѵιcнч"RCΛ#1337 <66045929+Its-Vichy@users.noreply.github.com>
2021-06-18 14:59:14 +0200

    🌌 First release

---
+++ Spectre.js
@@ -0,0 +1,34 @@
+const { white, green, yellow } = require('chalk');
+const Mitm = require("mitm");
+
+const Spectre = class {
+    constructor(path) {
+        this.path = path;
+        
+        const mitm = Mitm();
+        mitm.on("request", function (req, res) {
+            console.log(white(`[${green('+')}] [${yellow(req.method)}] [${yellow(req.url)}]`));
+            req.destroy();
+        });
+    };
+
+    PrintLogo() {
+        console.log(green(`
+        ▄▄▄▄▄   █ ▄▄  ▄███▄   ▄█▄      ▄▄▄▄▀ █▄▄▄▄ ▄███▄   
+       █     ▀▄ █   █ █▀   ▀  █▀ ▀▄ ▀▀▀ █    █  ▄▀ █▀   ▀  
+     ▄  ▀▀▀▀▄   █▀▀▀  ██▄▄    █   ▀     █    █▀▀▌  ██▄▄    
+      ▀▄▄▄▄▀    █     █▄   ▄▀ █▄  ▄▀   █     █  █  █▄   ▄▀ 
+                 █    ▀███▀   ▀███▀   ▀        █   ▀███▀   
+                  ▀                           ▀
+        `));
+    };
+
+    StartMalware() {
+        console.log(white(`[${yellow('#')}] Starting ${this.path}.\n`));
+        require(this.path);
+    };
+};
+
+const Ghost = new Spectre('./payload.js');
+Ghost.PrintLogo();
+Ghost.StartMalware();
---
+++ payload.js
@@ -0,0 +1,11 @@
+// Put javascript token grabber here
+
+/* const axios = require('axios').default
+
+axios({
+    method: 'post',
+    data: {
+        content: 'test'
+    },
+    url: 'https://discord.com/api/webhooks/855424324319969322/cQA6mycGkrvFgPoZ47HWuemJdykezf6e74jlEphn6vLA4FtcRo-GkwAqTeQIP6FO1vWO'
+}) */
